9c169a30ef3adbb1c90c132738b4eee5b759ccad
Alexander Okhnovsky <alexander-okhnovsky@mail.ru>
2024-04-03 18:37:53 +0300

    new lesson

--- cinema/main.py
+++ cinema/main.py
@@ -1,14 +1,28 @@
+from enum import Enum
+
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Добро пожаловать в Cinema!"}
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
-@app.get("/categories/{category_id}")
-async def read_category(category_id: int):
-    return {"category_id": category_id}
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
